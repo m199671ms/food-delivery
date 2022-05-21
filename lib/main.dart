7ff6a5c9aa7a01051f516567c29df84e63f86f00
Moe Saeed <m.199671.ms@gmail.com>
2022-05-21 18:09:16 +0200

    Deleat Exception line

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/pages/food/popular_food_detail.dart';
-import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
